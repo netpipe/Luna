9ec649b60e7ac40f974a696814d0b77842e1cee5
teslaenergy@gmail.com <teslaenergy@gmail.com>
2019-12-28 12:16:24 -0700

    emscripten

---
+++ config.h
@@ -0,0 +1,45 @@
+    #define IRRCD   // irrlicht Collision Detection
+   // #define IRRc
+ //   #define PostProcess
+  //  #define ReflectiveWater
+  //  #define ReflectWater-Main
+#define DPHYSICS
+#define PHYSICS
+    #define TREES //very cpu intensive
+    #define ATMOSPHERE  //  freezes when no video acceleration is used
+                        //  or as a user without access to video rights is used
+    //  #define FLARE
+  //  #define FLARE2  // more realistic and working
+
+   // #define DECALS2       // simple decals
+    //#define DECALS       //decalmanager sortof working slow tho
+  //  #define TESSIMAGE
+
+  //  #define EDITOR
+    #define FLAG    //
+  //  #define FLAG2   //FMX
+   // #define COMPASS
+  //  #define BOIDS
+    #define TERRAIN
+  //  #define occlusion
+  //  #define FPSWEAPON // no uses diff event receiver than player so wont drive car or shoot cubes // screws with the flare2
+    #define BULLETCAR // if FPSWEAPON is enabled the car wont work. diff event rec
+    #define LOADLEVELS // dont use this without bullet or with occlusion its slow?
+  //  #define BULLETBLEND
+  //  #define RAG   //bulletRagdoll
+   // #define EXTRAS
+  //  #define DESTRUCTION
+  //  #define FORMATIONDEMO
+  //  #define HUD
+    #define Image2D // Just testing out
+  //  #define CHOPPER
+  //  #define  OPENSTEER
+  //  #define SQLITE
+//  #define VEGETATION
+//  #define SCALC
+//#define FT2
+//#define SPRITES
+
+
+//    #define VIDEO
+  //  #define PostProcess
