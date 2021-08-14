e086aea12f7475af605c1341e545a0ee21f33530
netpipe <teslaenergy@gmail.com>
2021-08-13 19:28:13 -0600

    demo

--- Physics/Vehicle.cpp
+++ Physics/Vehicle.cpp
@@ -500,7 +500,7 @@ else if(driveType==5){ // Bikes
 
     for (int i = 0; i < m_vehicle->getNumWheels(); i++)    {
         tempNode = m_irrDevice->getSceneManager()->addAnimatedMeshSceneNode(
-        m_irrDevice->getSceneManager()->getMesh("./media/models/vehicles/wheel.x") );
+        m_irrDevice->getSceneManager()->getMesh("../media/models/vehicles/wheel.x") );
    //     if (i=0){
         tempNode->setMaterialTexture(0, m_irrDevice->getVideoDriver()->getTexture("./media/models/vehicles/TIRE_TEX.TGA"));
     //    };
