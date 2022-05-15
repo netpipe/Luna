eb0747a439bd5262343f346e3b02a8ee337b54eb
netpipe <teslaenergy@gmail.com>
2022-05-14 22:14:27 -0600

    windowsfixes

--- Luna.cpp
+++ Luna.cpp
@@ -530,6 +530,9 @@ int Luna::Run(){  // starts the game in dev mode or release mode some features a
   //        device->getFileSystem()->addFileArchive("..\\media\\pydata.zip");
 // Py_SetPythonHome( "pydata" ); // needs fixing still
            Py_SetPythonHome( "..\\media\\"); // needs fixing still
+           if std::filesystem::exists("..\\media\\pydata\\"){
+           Py_SetPythonHome( "..\\media\\pydata\\"); // needs fixing still
+           }
             //PySys_SetPath( 'C:/Dev/Luna/media/pydata/lib/'
             #endif
         #endif
