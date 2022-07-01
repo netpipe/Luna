e9a707a100a931bc19929ddfb6bcf15329e946d4
User <user@Users-MBP.lan>
2022-06-30 19:46:24 -0600

    osxdeploy

--- GAME/macDeploy.sh
+++ GAME/macDeploy.sh
@@ -1,12 +1,28 @@
 #!/bin/bash
-
-#check if directory exists media otherwise copy it into luna.app framework
+#this script is for code blocks
 if [ ! -d "./GAME/Luna.app/Contents/media" ]
 then
 echo "copying media folder"
 cp -R ./media ./GAME/Luna.app/Contents/
 fi
 echo "copying binary"
+if [ ! -d "./GAME/mLunaF" ]
+then
+echo "found build Copying file"
 cp ./GAME/mLunaF ./GAME/Luna.app/Contents/MacOS/GAME/mLuna
+else
+echo "minimal build ?"
+fi
+if [ ! -d "./GAME/mLuna" ]
+then
+echo "yes it was minimal build"
+cp ./GAME/mLunaF ./GAME/Luna.app/Contents/MacOS/GAME/mLuna
+else
+echo "rebuild , no binarys present"
+fi
+
 echo "running APP"
 open ./GAME/Luna.app
+echo "cleaning up"
+rm ./GAME/mLuna
+rm ./GAME/mLunaF
