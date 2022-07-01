fd3b49728f07978fcf61d172b48e23cd55690ffb
User <user@Users-MBP.lan>
2022-06-30 19:32:02 -0600

    osxdeploy

--- GAME/macDeploy.sh
+++ GAME/macDeploy.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 #check if directory exists media otherwise copy it into luna.app framework
-if [ -d "./GAME/Luna.app/Contents/MacOS/GAME/mLuna"] then
-cp ./media ./GAME/Luna.app/Contents/media
-
+if [ ! -d "./GAME/Luna.app/Contents/media" ]
+then
+echo "copying media folder"
+cp -R ./media ./GAME/Luna.app/Contents/
+fi
+echo "copying binary"
 cp ./GAME/mLunaF ./GAME/Luna.app/Contents/MacOS/GAME/mLuna
-
+echo "running APP"
 open ./GAME/Luna.app
