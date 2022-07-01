80190ca31f7b0525bf9a4e11955bf60bdcf9ac0d
User <user@Users-MBP.lan>
2022-06-30 19:20:45 -0600

    lunaapp

--- GAME/macDeploy.sh
+++ GAME/macDeploy.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+#check if directory exists media otherwise copy it into luna.app framework
+if [ -d "./GAME/Luna.app/Contents/MacOS/GAME/mLuna"] then
+cp ./media ./GAME/Luna.app/Contents/media
+
 cp ./GAME/mLunaF ./GAME/Luna.app/Contents/MacOS/GAME/mLuna
 
 open ./GAME/Luna.app
