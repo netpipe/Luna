fb2fd7ce4d3529197d29c8bd907bf269d2a56fba
netpipe <teslaenergy@gmail.com>
2023-02-25 10:49:14 -0700

    racing

--- media/lib/trig.py
+++ media/lib/trig.py
@@ -82,3 +82,37 @@ def factorial(x):
         product *= i
     return product
 
+
+def atan2(x,y):
+    if x==0:
+        if y>0:
+            return PI/2
+        else:
+            return -PI/2
+    
+    slope=y/x
+    result=0
+    if slope>=1:
+        result=PI/2
+        for i in range(40):
+            result-=((-1)**i)/((2*i+1)*slope**(2*i+1))
+    elif slope <=-1:
+        result=-PI/2
+        for i in range(40):
+            result-=((-1)**i)/((2*i+1)*slope**(2*i+1))
+    else:
+        result=0
+        for i in range(40):
+            result+=((-1)**i)*(slope**(2*i+1))/(2*i+1)
+
+    if x<0:
+        if y>=0:
+            result+=PI
+        else:
+            result-=PI
+    result%=2*PI
+    if result>PI:
+        result-=2*PI
+    elif result<-PI:
+        result+=2*PI
+    return result
